4285cd1deff96291aab7c37604d9a702d4430d5f
memo <memerodriguezavila@gmail.com>
2026-06-24 23:35:39 -0600

    feat:implementacion de boton STOP, visualizacion de pi pico w conectada, mejor estilo visual, implementacion de botones de maxima y frecuencia media

--- server.js
+++ server.js
@@ -3,54 +3,31 @@ const http = require('http');
 const { Server } = require('socket.io');
 const net = require('net');
 
-// 1. Configuracion del Servidor Web (Para tu telefono)
 const app = express();
 const serverHttp = http.createServer(app);
 const io = new Server(serverHttp);
 
-// Servir archivos estaticos de la carpeta 'public' (Aqui evitamos el internet!)
 app.use(express.static('public'));
 
-// 2. Configuraci�n del Servidor TCP (Para la Pico W)
-let picoSocket = null; // Guardaremos la conexion de la Pico W aqui
+let picoSocket = null;
 
 const serverTCP = net.createServer((socket) => {
-    console.log('Pico W conectada al hardware!');
+    console.log('Pico W conectada!');
     picoSocket = socket;
-
-    socket.on('error', (err) => {
-        console.log('Error en la Pico W:', err.message);
-    });
+    io.emit('estado_pico', { conectada: true });
 
     socket.on('close', () => {
-        console.log('Pico W desconectada');
         picoSocket = null;
+        io.emit('estado_pico', { conectada: false });
     });
 });
 
-// 3. El Puente de Comunicacion (Telefono <--> Servidor <--> Pico W)
 io.on('connection', (clienteWeb) => {
-    console.log('Telefono conectado a la interfaz web');
-
-    // Cuando el telefono aprieta un boton en la pagina:
+    clienteWeb.emit('estado_pico', { conectada: picoSocket !== null });
     clienteWeb.on('comando_generador', (instruccion) => {
-        console.log('Orden desde el telefono:', instruccion);
-        
-               // Si la Pico W esta conectada, le rebotamos el mensaje inmediatamente
-        if (picoSocket) {
-            // Convertimos a String por seguridad y le pegamos el salto de l�nea
-            picoSocket.write(String(instruccion) + '\n'); 
-        } else {
-            console.log('Aviso: La orden no se envi�, Pico W no conectada');
-        }
+        if (picoSocket) picoSocket.write(String(instruccion) + '\n');
     });
 });
 
-// 4. Encender los motores
-serverTCP.listen(8080, () => {
-    console.log('Servidor Hardware (Pico W) escuchando en puerto 8080');
-});
-
-serverHttp.listen(3000, () => {
-    console.log('Servidor Web (Telefono) escuchando en puerto 3000');
-});
+serverTCP.listen(8080);
+serverHttp.listen(3000, () => console.log('Sistema AWG listo en puerto 3000'));
